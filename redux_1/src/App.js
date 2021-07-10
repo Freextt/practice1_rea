5bb7997112f8b977ef57ec4f84ab1bd34c331aab
Freextt <olehsam@gmail.com>
2021-07-11 00:43:17 +0300

    Update

--- redux_1/src/App.js
+++ redux_1/src/App.js
@@ -1,24 +1,43 @@
 import logo from './logo.svg';
 import './App.css';
-
+import {useSelector,useDispatch} from "react-redux";
 function App() {
-
+  const SomeNestedChildComponent = () => {
+      // eslint-disable-next-line react-hooks/rules-of-hooks
+    const counter = useSelector(({count}) => count);
+    console.log(counter, 'cuwniwnc');
+      // eslint-disable-next-line react-hooks/rules-of-hooks
+    const dispatch = useDispatch();
+    return (
+        <header className="App-header">
+            <h1>{counter}</h1>
+            <button onClick={()=>{
+            dispatch({type: 'INC', payload: +prompt()});
+            }
+            }>INCREASE</button>
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+              className="App-link"
+              href="https://reactjs.org"
+              target="_blank"
+              rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+    )
+  }
+const SomeChildComponent = () => {
+    return(
+        <SomeNestedChildComponent/>
+    )
+}
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <SomeChildComponent/>
     </div>
   );
 }
--- redux_1/src/index.js
+++ redux_1/src/index.js
@@ -4,23 +4,36 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import {createStore} from 'redux';
+import {Provider} from "react-redux";
 const initialState = {
     count: 0
 }
 const countReduce = (state = initialState, action) => {
     switch (action.type){
-        case "INC":
-        case "INC CUSTOM":
-        case "DECREASE":
-        case "RESET":
+        case "INC":{
+            return {...state,count: state.count + 1}
+        }
+        case "INC CUSTOM":{
+            return {...state,count: state.count + action.payload}
+        }
+        case "DECREASE":{
+            return {...state,count: state.count - 1}
+        }
+        case "RESET":{
+            return {...state,count: state.count = 0}
+        }
         default:
             return state;
     }
 }
 const storage = createStore(countReduce);
+console.log(storage);
+console.log(storage.getState());
 ReactDOM.render(
   <React.StrictMode>
+      <Provider store={storage}>
     <App />
+      </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
