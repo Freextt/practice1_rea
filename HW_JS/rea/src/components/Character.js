4f5a4ad3102b5370b9bfb72a159d9259658fc53f
Freextt <olehsam@gmail.com>
2021-08-12 00:32:38 +0300

    React hw_1

---
+++ HW_JS/rea/src/components/Character.js
@@ -0,0 +1,10 @@
+import Simpsons from "./Simpsons";
+export default function Character({item:{name,surname,age,info,photo}}) {
+    return(
+        <div>
+            <h2>{name} {surname}. Age - {age}</h2>
+            <img src={photo} alt=""/>
+            <p>{info}</p>
+        </div>
+    )
+}
