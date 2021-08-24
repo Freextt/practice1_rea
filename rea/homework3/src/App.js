1966fdfa1aac0299a9b41278a8ed85c1a6a5ad16
Freextt <olehsam@gmail.com>
2021-08-24 16:21:23 +0300

    Homework3 + practice

---
+++ rea/homework3/src/App.js
@@ -0,0 +1,15 @@
+//import logo from './logo.svg';
+//import './App.css';
+
+import Users from "./components/users/Users";
+import Posts from "./components/posts/Posts";
+function App() {
+  return (
+    <div>
+      <Users/>
+      <Posts/>
+    </div>
+  );
+}
+
+export default App;
