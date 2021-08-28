e5701e5477ebd7fb1292e78bc76b48b68139925d
Freextt <olehsam@gmail.com>
2021-08-28 19:33:23 +0300

    Homework + practice 4

---
+++ rea/homework4/src/App.js
@@ -0,0 +1,13 @@
+//import logo from './logo.svg';
+//import './App.css';
+import Cars from "./components/cars/Cars";
+
+function App() {
+  return (
+   <div>
+     <Cars/>
+   </div>
+  );
+}
+
+export default App;
