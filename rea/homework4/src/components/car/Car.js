d88808f29faf8c94bdc5834d13666ebf75a1f244
Oleh Semyhinovskyy <83244335+Freextt@users.noreply.github.com>
2021-08-30 19:27:10 +0300

    Delete rea/homework4 directory

--- rea/homework4/src/components/car/Car.js
+++
@@ -1,9 +0,0 @@
-export default function Car({items:{id,model,price,year},deleteCar,editCar}){
-    return(
-        <div>
-            <h2>{id} - {model} - {price} - {year}</h2>
-            <button onClick={deleteCar(id)}>Delete</button>
-            <button onClick={editCar(id)}>Edit</button>
-        </div>
-    )
-}
